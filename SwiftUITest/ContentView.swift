1f57fe9264f1f643d97b88a711c06e273bad20a9
Quoc Nguyen <quocnb.vnu@gmail.com>
2019-07-04 12:54:45 +0900

    Initial Commit

---
+++ SwiftUITest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUITest
+//
+//  Created by Quoc Nguyen on 2019/07/04.
+//  Copyright © 2019 Quoc Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
