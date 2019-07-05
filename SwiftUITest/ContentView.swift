b91b7050ac46419e4d09cada9c5d91138d9d7b4f
Quoc Nguyen <quocnb.vnu@gmail.com>
2019-07-05 14:50:02 +0900

    Creating and Combining Views

--- SwiftUITest/ContentView.swift
+++ SwiftUITest/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        Text("Hello World")
+        VStack(alignment: .leading) {
+            Text("QuocNB").font(.title).color(.red)
+            HStack {
+                Text("Hello World").font(.subheadline).color(.green)
+                Spacer()
+                Text("I'm Vietnamese").font(.subheadline).color(.blue)
+            }
+        }.padding()
     }
 }
 
